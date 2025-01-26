14604eec58a064d9773d90be3765588234f2825a
Dmytro Matsiura <dmytromatsiura@gmail.com>
2025-01-26 12:04:03 +0200

    chore: add toastify package

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import router from '@/router'
 import vuetify from '@/plugins/vuetify'
 import setupInterceptors from '@/services/api/axios.interceptors'
 
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
+
 import { makeServer } from '@/server'
 if (import.meta.env.DEV) {
   makeServer()
@@ -19,5 +21,8 @@ const app = createApp(App)
 app.use(pinia)
 app.use(router)
 app.use(vuetify)
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+} as ToastContainerOptions)
 
 app.mount('#app')
