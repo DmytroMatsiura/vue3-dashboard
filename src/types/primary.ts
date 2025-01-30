48647bf3619be2f113dc270b98460d8892ad1ae7
Dmytro Matsiura <dmytromatsiura@gmail.com>
2025-01-30 22:22:18 +0200

    chore: add primary types

--- src/types/primary.ts
+++ src/types/primary.ts
@@ -1,1 +1,13 @@
 export type EmptyObject = Record<string, never>
+
+export type Nullable<T> = T | null
+
+export type Optional<T> = T | undefined
+
+export type ValueOf<T> = T[keyof T]
+
+export type EmptyString = ''
+
+export type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>
+}
