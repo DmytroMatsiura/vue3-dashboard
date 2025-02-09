9e0213c6859789aabd979e8b0ad3b5baf8ab5dc6
Dmytro Matsiura <dmytromatsiura@gmail.com>
2025-02-09 20:37:38 +0200

    feat: update default app layout

--- src/layouts/default/DefaultLayout.vue
+++ src/layouts/default/DefaultLayout.vue
@@ -1,11 +1,27 @@
 <template>
   <v-app>
-    <v-main>
-      <RouterView />
-    </v-main>
+    <v-layout class="rounded rounded-md">
+      <v-navigation-drawer>
+        <AppSidebar />
+      </v-navigation-drawer>
+
+      <v-app-bar>
+        <AppHeader />
+      </v-app-bar>
+
+      <v-main
+        class="d-flex align-center justify-center"
+        style="min-height: 300px"
+      >
+        <RouterView />
+      </v-main>
+    </v-layout>
   </v-app>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import AppHeader from './components/AppHeader.vue'
+  import AppSidebar from './components/AppSidebar.vue'
+</script>
 
 <style scoped></style>
