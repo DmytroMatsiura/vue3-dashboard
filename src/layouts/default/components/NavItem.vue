465e00edc5abaabfefabc4e44b1ddaa16d9767b4
Dmytro Matsiura <dmytromatsiura@gmail.com>
2025-02-14 20:59:38 +0200

    feat: update nav item and nav item expandable templates

--- src/layouts/default/components/NavItem.vue
+++ src/layouts/default/components/NavItem.vue
@@ -1,7 +1,40 @@
 <template>
-  <div>NavItem</div>
+  <v-list-item
+    :to="item.to"
+    link
+    class="nav-item"
+  >
+    <!-- Icon -->
+    <v-list-item-avatar v-if="item.icon">
+      <v-icon>{{ item.icon }}</v-icon>
+    </v-list-item-avatar>
+
+    <!-- Title -->
+    <v-list-item-title>
+      {{ item.title }}
+    </v-list-item-title>
+  </v-list-item>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import type { PropType } from 'vue'
 
-<style scoped></style>
+  interface NavItemType {
+    title: string
+    icon?: string
+    to?: string
+  }
+
+  defineProps({
+    item: {
+      type: Object as PropType<NavItemType>,
+      required: true,
+    },
+  })
+</script>
+
+<style scoped>
+  .nav-item {
+  cursor: pointer;
+}
+</style>
--- src/layouts/default/components/NavItemExpandable.vue
+++ src/layouts/default/components/NavItemExpandable.vue
@@ -1,7 +1,66 @@
 <template>
-  <div>NavItemExpandable.vue</div>
+  <v-list-group
+    prepend-icon="mdi-chevron-down"
+    :value="open"
+    @click:prepend="toggleGroup"
+    class="nav-item-expandable"
+  >
+    <!-- Activator Slot -->
+    <template #activator>
+      <v-list-item-avatar v-if="item.icon">
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-avatar>
+      <v-list-item-title>
+        {{ item.title }}
+      </v-list-item-title>
+    </template>
+
+    <!-- Children -->
+    <div
+      v-for="(child, index) in item.children"
+      :key="index"
+    >
+      <NavItemExpandable
+        v-if="child.children && child.children.length"
+        :item="child"
+      />
+      <NavItem
+        v-else
+        :item="child"
+      />
+    </div>
+  </v-list-group>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import type { PropType } from 'vue'
+  import NavItem from './NavItem.vue'
+  import NavItemExpandable from './NavItemExpandable.vue'
 
-<style scoped></style>
+  interface NavItemType {
+    title: string
+    icon?: string
+    to?: string
+    children?: NavItemType[]
+  }
+
+  defineProps({
+    item: {
+      type: Object as PropType<NavItemType>,
+      required: true,
+    },
+  })
+
+  const open = ref(false)
+
+  function toggleGroup() {
+    open.value = !open.value
+  }
+</script>
+
+<style scoped>
+  .nav-item-expandable {
+  cursor: pointer;
+}
+</style>
