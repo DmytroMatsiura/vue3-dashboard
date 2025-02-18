34d9cfb673027f264e7400c3cf8f981d71282e14
Dmytro Matsiura <dmytromatsiura@gmail.com>
2025-02-18 16:14:49 +0200

    chore: update prettier and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -22,4 +22,16 @@ export default [
     files: ['src/**/__tests__/*'],
   },
   skipFormatting,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+
+      'vue/html-self-closing': 'off',
+      'vue/require-default-prop': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/require-valid-default-prop': 'off',
+    },
+  },
 ]
